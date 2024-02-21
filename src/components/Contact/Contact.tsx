c42ff8b7ad3d6b7dcf85587142ea6fafda7a871f
Yan Kaiky <yankaikys@gmail.com>
2024-02-21 18:30:16 -0300

    feat(Contact): alter link to whatsapp

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -48,7 +48,7 @@ export const Contact = () => {
 
                 <motion.div variants={variants} className="item">
                     <h2>WhatsApp</h2>
-                    <span><a href="tel:5547999556723" target='_blank'>+55 (47) 99955-6723</a></span>
+                    <span><a href="https://wa.me/5547999556723" target='_blank'>+55 (47) 99955-6723</a></span>
                 </motion.div>
 
                 <motion.div variants={variants} className="item">
