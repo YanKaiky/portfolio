c4d623868c2f582774d1b42c1680af7bb99cc2d2
Yan Kaiky <yankaikys@gmail.com>
2025-02-09 16:01:47 -0300

    remove scss and add talwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { About, Contact, Cursor, Hero, Navbar, Parallax, Portfolio, Services } from './components';
-import './app.scss';
-
 const App = () => {
-  return (
-    <div>
-      <Cursor />
-      <section id='home'>
-        <Navbar />
-        <Hero />
-      </section>
-      <section id='about'><About /></section>
-      <section id='services'><Parallax type='services' /></section>
-      <section><Services /></section>
-      <section id='portfolio'><Parallax type='portfolio' /></section>
-      <Portfolio />
-      <section id='contact'><Contact /></section>
-    </div>
-  );
+  return <h1 className="font-bold text-red-800 underline">Test Text</h1>;
 };
 
 export default App;
